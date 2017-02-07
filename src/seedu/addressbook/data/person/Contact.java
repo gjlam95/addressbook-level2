67cdeaec0566c4674c678bf0d0a22a5eac445892
gjlam95 <gj_lam@hotmail.com>
2017-02-07 19:14:03 +0800

    Created Contact class. Extracted commonalities from Phone, Email and Address classes into Contact class.

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,36 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+
+    private final String value;
+    private boolean isPrivate;
+    
+    public Contact(String value, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        this.value = value;
+    }
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
